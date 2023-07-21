ff97d3676b4aac1e677fc67d01ee7c4e476946a2
FrancescoPepi <francescopepi16@gmail.com>
2023-07-21 20:13:28 +0200

    up style

--- JS/main.js
+++ JS/main.js
@@ -10,6 +10,8 @@ for (let i = 1; i < 101; i++) {
     const box = document.getElementById(`box-${i}`);
     console.log(box);
     box.style.backgroundColor = "#8cb369";
+    box.style.border = "1px solid yellow";
+
     // SE sono multipli di 5:
   } else if (i % 5 == 0 && !(i % 3 == 0)) {
     Container.innerHTML += `<div id="box-${i}" class="d-flex align-items-center justify-content-center">buzz</div>`;
@@ -17,6 +19,8 @@ for (let i = 1; i < 101; i++) {
     const box = document.getElementById(`box-${i}`);
     console.log(box);
     box.style.backgroundColor = "#f3a259";
+    box.style.border = "1px solid yellow";
+
     // SE sono multipli sia di 3 che di 5:
   } else if (i % 5 == 0 && i % 3 == 0) {
     Container.innerHTML += `<div id="box-${i}" class="d-flex align-items-center justify-content-center">FizzBuzz</div>`;
@@ -24,6 +28,8 @@ for (let i = 1; i < 101; i++) {
     const box = document.getElementById(`box-${i}`);
     console.log(box);
     box.style.backgroundColor = "#bc4b51";
+    box.style.border = "1px solid yellow";
+
     // Stampare una serie di numeri in console da 1 a 100
   } else {
     Container.innerHTML += `<div id="box-${i}" class="d-flex align-items-center justify-content-center">${i}</div>`;
@@ -31,5 +37,6 @@ for (let i = 1; i < 101; i++) {
     const box = document.getElementById(`box-${i}`);
     console.log(box);
     box.style.backgroundColor = "#7dd0b6";
+    box.style.border = "1px solid yellow";
   }
 }
